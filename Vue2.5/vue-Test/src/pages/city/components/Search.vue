a23fa56c9e7e594296a5a22c3cedda07317bbf2a
SheepHan <williamcheng0928@gmail.com>
2018-10-09 23:06:48 +0800

    添加城市搜索

--- Vue2.5/vue-Test/src/pages/city/components/Search.vue
+++ Vue2.5/vue-Test/src/pages/city/components/Search.vue
@@ -1,23 +1,69 @@
 <template>
   <div>
     <div class="search">
-      <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
-    <!-- <div class="search-content">
+    <div
+      class="search-content"
+      ref="search"
+      v-show="keyword"
+      >
       <ul>
-        <li class="search-item border-bottom">
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{item.name}}
         </li>
-        <li class="search-item border-bottom">
+        <li class="search-item border-bottom" v-show="hasNoData">
           没有找到匹配数据
         </li>
       </ul>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CityHeader'
+  name: 'CitySearch',
+  props: {
+    city: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.city) {
+          this.city[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  }
 }
 </script>
 
