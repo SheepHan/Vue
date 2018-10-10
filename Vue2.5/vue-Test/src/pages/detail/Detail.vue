99a175335e287ea66102fdd9be530b37a3f8818c
SheepHan <williamcheng0928@gmail.com>
2018-10-10 21:51:35 +0800

    add

---
+++ Vue2.5/vue-Test/src/pages/detail/Detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    detail
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
